b1bfc571c3004def25d037c7b14250b80a89cf7a
bcarlee <bcarlee@student.21-school.ru>
2022-04-18 09:18:59 +0300

    task 00

---
+++ day00/ex00/Program.java
@@ -0,0 +1,15 @@
+public class Program{
+    public static void main(String[] argv){
+        int number;
+        int result;
+
+        result = 0;
+        number = 546894;
+        result += number % 10;
+        while (number > 10){
+            number = number / 10;
+            result += number % 10;
+        }
+        System.out.println(result);
+    }
+}
