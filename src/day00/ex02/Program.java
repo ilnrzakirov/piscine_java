1ade3a7e649b53324f01973c469f11fdd5f5dbdf
bcarlee <bcarlee@student.21-school.ru>
2022-04-18 11:29:58 +0300

    task 02

--- src/day00/ex02/Program.java
+++ src/day00/ex02/Program.java
@@ -1,7 +1,54 @@
 package day00.ex02;
+import javafx.beans.binding.When;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] argv) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int amountCoffee = 0;
+        int sumOfDigit;
 
+        while (number != 42){
+            sumOfDigit = getSumOfDigit(number);
+            if (isPrime(sumOfDigit)){
+                amountCoffee++;
+            }
+            number = scanner.nextInt();
+        }
+        System.out.println("Count of coffee-request – " + amountCoffee);
+    }
+
+    public static int getSumOfDigit(int number){
+        int result = 0;
+
+        result += number % 10;
+        while (number > 10){
+            number = number / 10;
+            result += number % 10;
+        }
+        return (result);
+    }
+
+    public static boolean isPrime(int number){
+        int temp;
+
+        if (number <= 1){
+            return (false);
+        }
+        else if (number == 2){
+            return (true);
+        }
+        else{
+            for (int i = 2; i * i <= number + 1; i++){
+                temp = number % i;
+                if (temp == 0)
+                {
+                    return (false);
+                }
+            }
+        }
+        return (true);
     }
 }
