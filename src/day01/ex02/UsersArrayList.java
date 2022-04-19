4093132d098582cb2fc939bfa99e9342a75de73b
bcarlee <bcarlee@student.21-school.ru>
2022-04-19 13:46:59 +0300

    task2

--- src/day01/ex02/UsersArrayList.java
+++ src/day01/ex02/UsersArrayList.java
@@ -1,4 +1,25 @@
 package day01.ex02;
 
-public class UsersArrayList {
+public class UsersArrayList implements UserList {
+    private User[] users = new User[10];
+
+    @Override
+    public void addUser(User newUser) {
+
+    }
+
+    @Override
+    public User getUserById(Integer id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public User getUserByIndex(Integer index) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Integer getUserCount() {
+        return null;
+    }
 }
