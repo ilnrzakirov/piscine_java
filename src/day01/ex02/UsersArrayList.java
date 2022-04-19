f8222d16d2ea102172469e9aa7b5fb7bbb5214cb
bcarlee <bcarlee@student.21-school.ru>
2022-04-19 14:15:23 +0300

    task2

--- src/day01/ex02/UsersArrayList.java
+++ src/day01/ex02/UsersArrayList.java
@@ -2,26 +2,44 @@ package day01.ex02;
 
 public class UsersArrayList implements UserList {
     private User[] users = new User[10];
-    private static Integer count;
-    private static Integer arraySize = 10;
+    private int count;
+    private int arraySize = 10;
 
     @Override
     public void addUser(User newUser) {
-
+        if (this.count == this.arraySize){
+            User[] users = new User[arraySize * 2];
+            for (int i = 0; i < this.arraySize; i++){
+                users[i] = this.users[i];
+            }
+            this.arraySize = this.arraySize * 2;
+            this.users = users;
+            this.users[count++] = newUser;
+        } else {
+            this.users[count++] = newUser;
+        }
     }
 
     @Override
-    public User getUserById(Integer id) throws Exception {
-        return null;
+    public User getUserById(int id) {
+        for (int i = 0; i < this.count; i++){
+            if (users[i].getId() == id){
+                return users[i];
+            }
+        }
+        throw new UserNotFoundException("User not found");
     }
 
     @Override
-    public User getUserByIndex(Integer index) throws Exception {
+    public User getUserByIndex(int index) {
+        if (index <= this.count){
+            return users[index];
+        }
         return null;
     }
 
     @Override
     public Integer getUserCount() {
-        return UsersArrayList.count + 1;
+        return this.count + 1;
     }
 }
--- src/day01/ex02/UsersList.java
+++ src/day01/ex02/UsersList.java
@@ -2,7 +2,7 @@ package day01.ex02;
 
 interface UserList{
     void addUser(User newUser);
-    User getUserById(Integer id) throws Exception;
-    User getUserByIndex(Integer index) throws Exception;
+    User getUserById(int id);
+    User getUserByIndex(int index);
     Integer getUserCount();
 }
