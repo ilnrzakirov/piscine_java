e1accffe3856b4c2af9a8df7a52446de34b04a9c
bcarlee <bcarlee@student.21-school.ru>
2022-04-20 15:28:24 +0300

    task1

--- src/day02/ex00/Program.java
+++ src/day02/ex00/Program.java
@@ -1,16 +1,19 @@
 package day02.ex00;
 
+import javafx.beans.binding.When;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Program {
 
     private static final String END = "42";
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Map <String, String> signature = new HashMap<>();
         FileInputStream fileInputStream;
         FileOutputStream fileOutputStream;
@@ -21,7 +24,16 @@ public class Program {
         } catch (FileNotFoundException error){
             System.err.println("file not found");
         }
-
-
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+        while (inputLine.equals(END)){
+            try {
+                FileInputStream inputFile = new FileInputStream(inputLine);
+            } catch (FileNotFoundException error){
+                System.err.println("file not found");
+            }
+            inputLine = scanner.nextLine();
+        }
+        scanner.close();
     }
 }
