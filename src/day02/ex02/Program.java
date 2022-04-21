ae14c1d45a5044533888e8b60c91f49a51e59b99
bcarlee <bcarlee@student.21-school.ru>
2022-04-21 09:18:43 +0300

    task02

---
+++ src/day02/ex02/Program.java
@@ -0,0 +1,26 @@
+package day02.ex02;
+
+import java.io.File;
+
+public class Program {
+
+    private static final String ERRORDIR = "Error: directory not found";
+
+    public static void main(String[] args) {
+        if (args.length != 1 || !args[0].startsWith("--current-folder")){
+            System.err.println("invalid command");
+            System.exit(-1);
+        }
+
+        String inputPath = args[0].replaceFirst("--current-folder=", "");
+        if (inputPath.isEmpty()){
+            System.err.println(ERRORDIR);
+        }
+        File directory = new File(inputPath);
+
+        if (!directory.isDirectory()){
+            System.err.println(ERRORDIR);
+            System.exit(-1);
+        }
+    }
+}
