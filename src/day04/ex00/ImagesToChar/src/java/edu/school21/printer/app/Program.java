6d3b9c5fbad027de39f2a7cf13fb33d209eb7d61
bcarlee <bcarlee@student.21-school.ru>
2022-04-22 10:52:33 +0300

    day04

---
+++ src/day04/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -0,0 +1,8 @@
+package day04.ex00.ImagesToChar.src.java.edu.school21.printer.app;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day04/ex00/ImagesToChar/src/java/edu/school21/printer/logic/ImagesToChar.java
@@ -0,0 +1,4 @@
+package day04.ex00.ImagesToChar.src.java.edu.school21.printer.logic;
+
+public class ImagesToChar {
+}
