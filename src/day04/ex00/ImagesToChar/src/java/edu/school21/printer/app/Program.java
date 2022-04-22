d13605f114c854036d55533ecc9285f3fe6599d7
bcarlee <bcarlee@student.21-school.ru>
2022-04-22 11:17:28 +0300

    ex00

--- src/day04/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
+++ src/day04/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -1,8 +1,19 @@
 package day04.ex00.ImagesToChar.src.java.edu.school21.printer.app;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Program {
 
+    private static final String PATH_TO_BMP = "/Users/bcarlee/Desktop/Project/piscine_java/src/day04/it.bmp";
+    private static final String FILENOTFOUND = "File not found";
+
     public static void main(String[] args) {
+        try (FileInputStream fileInputStream = new FileInputStream(PATH_TO_BMP)) {
+            Integer in;
+        } catch (IOException error) {
+            System.err.println(FILENOTFOUND);
+        }
 
     }
 }
--- src/day04/ex00/ImagesToChar/src/java/edu/school21/printer/logic/ImagesToChar.java
+++ src/day04/ex00/ImagesToChar/src/java/edu/school21/printer/logic/ImagesToChar.java
@@ -1,4 +1,15 @@
 package day04.ex00.ImagesToChar.src.java.edu.school21.printer.logic;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ImagesToChar {
+    private BufferedImage image;
+
+    public ImagesToChar(FileInputStream imageFile) throws IOException {
+        this.image = ImageIO.read(imageFile);
+    }
 }
