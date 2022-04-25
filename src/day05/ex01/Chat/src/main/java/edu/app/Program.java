85377c1a7edbefb8c785c08e829180b5926e0e23
bcarlee <bcarlee@student.21-school.ru>
2022-04-25 15:08:13 +0300

    ex00 complete

---
+++ src/day05/ex01/Chat/src/main/java/edu/app/Program.java
@@ -0,0 +1,8 @@
+package edu.app;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day05/ex01/Chat/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO chat.users (login, password) VALUES ('Jack', 'jaja');
+INSERT INTO chat.users (login, password) VALUES ('Tim', 'titi');
+INSERT INTO chat.users (login, password) VALUES ('Tom', 'toto');
+INSERT INTO chat.users (login, password) VALUES ('John', 'jojo');
+INSERT INTO chat.users (login, password) VALUES ('Ben', 'bebe');
+
+INSERT INTO chat.rooms (name, owner) VALUES ('car', 1);
+INSERT INTO chat.rooms (name, owner) VALUES ('hobby', 2);
+INSERT INTO chat.rooms (name, owner) VALUES ('sport', 3);
+INSERT INTO chat.rooms (name, owner) VALUES ('stalker', 4);
+INSERT INTO chat.rooms (name, owner) VALUES ('animals', 5);
+
+
+INSERT INTO chat.message (author, chatroom, text, time) VALUES (1, 1, 'BMW car th', current_timestamp);
+INSERT INTO chat.message (author, chatroom, text, time) VALUES (2, 2, 'painter other s', current_timestamp);
+INSERT INTO chat.message (author, chatroom, text, time) VALUES (3, 3, 'bol sdf', current_timestamp);
+INSERT INTO chat.message (author, chatroom, text, time) VALUES (4, 4, 'comp stalker', current_timestamp);
+INSERT INTO chat.message (author, chatroom, text, time) VALUES (5, 5, 'cat and dog', current_timestamp);
+INSERT INTO chat.message (author, chatroom, text, time) VALUES (5, 5, 'horse and dog', current_timestamp);
