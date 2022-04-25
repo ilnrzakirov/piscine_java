1ba5d25d806b235195d521e9ec523081a4095b7f
bcarlee <bcarlee@student.21-school.ru>
2022-04-25 10:11:59 +0300

    created db models

--- src/day05/ex00/Chat/src/main/java/edu/models/Chatroom.java
+++ src/day05/ex00/Chat/src/main/java/edu/models/Chatroom.java
@@ -1,8 +1,10 @@
 package edu.models;
 
+import java.util.List;
+
 public class Chatroom {
     private long id;
     private String name;
     private String owner;
-
+    private List<Message> massageList;
 }
--- src/day05/ex00/Chat/src/main/java/edu/models/User.java
+++ src/day05/ex00/Chat/src/main/java/edu/models/User.java
@@ -1,8 +1,12 @@
 package edu.models;
 
+import java.util.List;
+
 public class User {
 
     private long id;
     private String login;
     private String password;
+    private List<Chatroom> myCreatedRooms;
+    private List<Chatroom> mySocializesRooms;
 }
