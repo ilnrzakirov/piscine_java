9bc0dcabd537ebbbc6bfa710b1424cd449770a54
bcarlee <bcarlee@student.21-school.ru>
2022-04-25 13:43:06 +0300

    ex00 complete

--- src/day05/ex00/Chat/src/main/java/edu/models/Message.java
+++ src/day05/ex00/Chat/src/main/java/edu/models/Message.java
@@ -1,6 +1,7 @@
 package edu.models;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Message {
 
@@ -9,4 +10,28 @@ public class Message {
     private Chatroom room;
     private String text;
     private LocalDateTime dateTime;
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", author=" + author +
+                ", room=" + room +
+                ", text='" + text + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return id == message.id && Objects.equals(author, message.author) && Objects.equals(room, message.room) && Objects.equals(text, message.text) && Objects.equals(dateTime, message.dateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, room, text, dateTime);
+    }
 }
--- src/day05/ex00/Chat/src/main/java/edu/school21/chat/Main.java
+++ src/day05/ex00/Chat/src/main/java/edu/school21/chat/Main.java
@@ -1,8 +1,95 @@
 package edu.school21.chat;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String DB_URL = "jdbc:postgresql://localhost/postgres";
+    private static final String USER = "postgres";
+    private static final String PASS = "";
+    private static final String PATH_SC = "src/main/resources/schema.sql";
+    private static final String PATH_DB = "src/main/resources/data.sql";
 
+    public static void main(String[] args) {
+        Connection connection = connect();
+
+        if (connection == null){
+            System.err.println("Failed to make connection to database");
+        } else {
+            System.out.println("Ok");
+        }
+
+        testExecute(connection);
+    }
+
+    private static void testExecute(Connection connection) {
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        List<String> allLineSchema = null;
+        List<String> allLineData = null;
+
+        try {
+            allLineSchema = Files.readAllLines(Paths.get(PATH_SC));
+            allLineData = Files.readAllLines(Paths.get(PATH_DB));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        if (allLineData  == null || allLineSchema == null){
+            System.err.println("Empty file");
+        }
+
+        for (String line : allLineSchema) {
+
+            try {
+                statement.execute(line);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+        for (String line : allLineData) {
+
+            try {
+                statement.execute(line);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+    }
+
+    private static Connection connect() {
+        Connection connection = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (ClassNotFoundException error) {
+            System.err.println(error.getMessage());
+            System.exit(-1);
+        } catch (SQLException error) {
+            System.err.println("PostgreSQL JDBC Driver is not found");
+            System.exit(-1);
+        }
+
+        return connection;
     }
 }
