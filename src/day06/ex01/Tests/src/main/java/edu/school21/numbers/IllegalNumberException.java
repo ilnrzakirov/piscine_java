fa6194d519a0aabfe12e41cefa6bff13ce68bdac
bcarlee <bcarlee@student.21-school.ru>
2022-04-26 12:15:37 +0300

    ex02

---
+++ src/day06/ex01/Tests/src/main/java/edu/school21/numbers/IllegalNumberException.java
@@ -0,0 +1,9 @@
+package edu.school21.numbers;
+
+public class IllegalNumberException extends RuntimeException {
+
+    public IllegalNumberException(String message){
+        super(message);
+    }
+
+}
---
+++ src/day06/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
@@ -0,0 +1,4 @@
+package edu.school21.repositories;
+
+public class EmbeddedDataSourceTest {
+}
