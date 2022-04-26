46929969fb76e4c43157ea38ddc491685854d29b
bcarlee <bcarlee@student.21-school.ru>
2022-04-26 14:34:39 +0300

    ex02

--- src/day06/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
+++ src/day06/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
@@ -1,4 +1,28 @@
 package edu.school21.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class EmbeddedDataSourceTest {
+
+    private DataSource dataSource;
+
+    @BeforeEach
+    public void init(){
+        EmbeddedDatabaseBuilder databaseBuilder = new EmbeddedDatabaseBuilder();
+        dataSource = databaseBuilder.addScript("schema.sql").addScript("data.sql").build();
+    }
+
+    @Test
+    void connectionTest() throws SQLException{
+        assertNotNull(dataSource.getConnection());
+    }
 }
