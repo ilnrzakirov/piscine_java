58953e7ad2f24483c41ecf39bf3e05e072c134a0
bcarlee <bcarlee@student.21-school.ru>
2022-04-27 17:39:47 +0300

    ex03

---
+++ src/day07/ex02/ORM/src/main/java/app/OrmManager.java
@@ -0,0 +1,16 @@
+package app;
+
+public class OrmManager {
+
+    public void save(Object entity){
+
+    }
+
+    public void update(Object entity){
+
+    }
+
+    public <T> T findById(Long id, Class<T> aClass){
+
+    }
+}
