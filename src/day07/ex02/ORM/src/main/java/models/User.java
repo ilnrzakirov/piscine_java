b74ea834847ed7b914a2cbf3bbeba256c95b5913
bcarlee <bcarlee@student.21-school.ru>
2022-04-27 17:31:36 +0300

    ex03

---
+++ src/day07/ex02/ORM/src/main/java/models/User.java
@@ -0,0 +1,49 @@
+package models;
+
+@OrmEntity(table = "simple_user")
+public class User {
+
+    @OrmColumnId
+    private Long id;
+
+    @OrmColumn(name = "first_name", length = 10)
+    private String firstName;
+
+    @OrmColumn(name = "first_name", length = 10)
+    private String lastName;
+
+    @OrmColumn(name = "age")
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
