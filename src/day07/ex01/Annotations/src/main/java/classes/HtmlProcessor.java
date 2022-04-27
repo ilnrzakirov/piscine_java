acb1e02202e3e66a7daab30a661ddd77adb7490a
bcarlee <bcarlee@student.21-school.ru>
2022-04-27 15:53:02 +0300

    all

---
+++ src/day07/ex01/Annotations/src/main/java/classes/HtmlProcessor.java
@@ -0,0 +1,14 @@
+package classes;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+public class HtmlProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
---
+++ src/day07/ex01/Annotations/src/main/java/classes/UserForm.java
@@ -0,0 +1,13 @@
+package classes;
+
+@HtmlForm(fileName = "user_form.html", action = "/users", method = "post")
+public class UserForm {
+    @HtmlInput(type = "text", name = "first_name", placeholder = "Enter First Name")
+    private String firstName;
+
+    @HtmlInput(type = "text", name = "last_name", placeholder = "Enter Last Name")
+    private String lastName;
+
+    @HtmlInput(type = "password", name = "password", placeholder = "Enter Password")
+    private String password;
+}
