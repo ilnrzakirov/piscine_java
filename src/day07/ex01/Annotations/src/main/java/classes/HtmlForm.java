610d134781e8ee7040548bad6a1074adb701209a
bcarlee <bcarlee@student.21-school.ru>
2022-04-27 15:53:33 +0300

    all

---
+++ src/day07/ex01/Annotations/src/main/java/classes/HtmlForm.java
@@ -0,0 +1,15 @@
+package classes;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface HtmlForm {
+    String fileName();
+    String action();
+    String method();
+}
