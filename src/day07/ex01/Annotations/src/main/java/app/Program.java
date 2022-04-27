8d73621645193b575fa2424d6973efe8392b5ee8
bcarlee <bcarlee@student.21-school.ru>
2022-04-27 16:32:23 +0300

    all

---
+++ src/day07/ex01/Annotations/src/main/java/app/Program.java
@@ -0,0 +1,11 @@
+package app;
+
+import classes.UserForm;
+
+public class Program {
+
+    public static void main(String[] args) {
+        UserForm userForm = new UserForm();
+
+    }
+}
--- src/day07/ex01/Annotations/src/main/java/classes/HtmlProcessor.java
+++ src/day07/ex01/Annotations/src/main/java/classes/HtmlProcessor.java
@@ -1,14 +1,33 @@
 package classes;
 
+import com.google.auto.service.AutoService;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
 
+@SupportedAnnotationTypes({"HtmlForm", "HtmlInput"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Process.class)
 public class HtmlProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (annotations.isEmpty()) {
+            return false;
+        }
+        for (TypeElement annotation : annotations) {
+            Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(annotation);
+            for (Element element : elements) {
+                TypeElement typeElement = (TypeElement) element.getEnclosingElement();
+
+            }
+        }
         return false;
     }
 }
